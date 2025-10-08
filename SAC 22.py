056d953080bab0422fd017fc097a442c5773ff84
jerrf010 <huntingaiden513@gmail.com>
2025-10-08 15:32:51 -0400

    Update

--- SAC 22.py
+++ SAC 22.py
@@ -1,14 +1,15 @@
-w = str(input("Enter the wordle guess: ").lower())
-amount_of_vowels = 0
-if len(w) == 5:
-    for i in range(len(w)):
-        if w[i] == "a" or w[i] == "e" or w[i] == "i" or w[i] == "o" or w[i] == "u":
-            amount_of_vowels += 1
+while True:
+    w = str(input("Enter the wordle guess: ").lower())
+    amount_of_vowels = 0
+    if len(w) == 5:
+        for i in range(len(w)):
+            if w[i] == "a" or w[i] == "e" or w[i] == "i" or w[i] == "o" or w[i] == "u":
+                amount_of_vowels += 1
 
-    if amount_of_vowels >= 2:
-        print("Good")
+        if amount_of_vowels >= 2:
+            print("Good")
+        else:
+            print("Bad")
+
     else:
-        print("Bad")
-
-else:
-    print("Error: Word must be 5 letters")
+        print("Error: Word must be 5 letters")
