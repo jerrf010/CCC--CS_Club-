3551bf76482123c3b6275c239525ed47b88534be
jerrf010 <huntingaiden513@gmail.com>
2025-10-29 15:01:23 -0400

    Update

--- 2d arrays.py
+++ 2d arrays.py
@@ -1,10 +1,18 @@
-arr = []
-distance = []
+arr = [[16,3,2,13],[5,10,11,8],[9,6,7,12],[4,15,14,1]]
+counter = 0
+rowsum = [0,0,0,0]
 for i in range(4):
-    i = int(input("Enter a number: "))
-    arr.append(i)
+    for f in range(4):
+        rowsum[i] += arr[i][f]
 
-for f in range(3):
-    distance.append(arr[f]+arr[f+1])
+    if rowsum[i] == 34:
+        counter += 1
+    for j in range(4):
+        colsum = 0
+        for k in range(4):
+            colsum += arr[k][j]
+        if colsum == 34:
+            counter += 1
 
-print(distance)
+if counter == 8:
+    print("Magic Square")
