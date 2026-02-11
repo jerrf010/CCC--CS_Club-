38028b0831df09d66bfa1ede139c1614ce2219b8
jerrf010 <huntingaiden513@gmail.com>
2026-02-11 16:45:12 -0500

    Final Solution

--- CCC/2025 J4.py
+++ CCC/2025 J4.py
@@ -4,7 +4,6 @@ days = []
 i = 0
 consecutive = 0
 left = ""
-done = False
 final = 0
 
 for _ in range(n):
@@ -14,13 +13,15 @@ for i in range(n):
     if days[i] == 'S':
         consecutive += 1
     else:
-        if days[i] == 'P':
-            if days[i + 1] == 'S' and not done:
+        if days[i] == 'P' and i !=0:
+            if days[i + 1] == 'S' and days[i - 1] == 'S':
                 consecutive += 1
                 done = True
             else:
                 consecutive = 0
+        else:
+            consecutive = 0
     if consecutive > final:
         final = consecutive
 
-print(consecutive)
+print(final)
