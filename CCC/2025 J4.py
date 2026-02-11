a89f6488b3c8e53a8ada202377de2f47be063c41
jerrf010 <huntingaiden513@gmail.com>
2026-02-11 15:57:22 -0500

    Update

--- CCC/2025 J4.py
+++ CCC/2025 J4.py
@@ -1,7 +1,26 @@
 n = int(input())
 
+days = []
+i = 0
+consecutive = 0
+left = ""
+done = False
+final = 0
+
 for _ in range(n):
-    days = int(input())
-    i = 0
-    while i < days:
-        
+    days.append(str(input()))
+
+for i in range(n):
+    if days[i] == 'S':
+        consecutive += 1
+    else:
+        if days[i] == 'P':
+            if days[i + 1] == 'S' and not done:
+                consecutive += 1
+                done = True
+            else:
+                consecutive = 0
+    if consecutive > final:
+        final = consecutive
+
+print(consecutive)
